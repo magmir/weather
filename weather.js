a3ccdf41d36d29acf4c7b8fecc655aaa33e81883
mamir <magda.miranowicz@gmail.com>
2015-11-22 17:08:42 +0000

    Identation

--- weather.js
+++ weather.js
@@ -1,17 +1,15 @@
-$(document).ready( function () {
-
-$('button').on ('click', function () {
-$.ajax({
-type: 'GET',
-url: 'http://api.openweathermap.org/data/2.5/weather?q=Edinburgh&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
-success: function weather (data) {
-console.log(data);
-alert('Location: ' + data.name + '\n' +
-    'Temperature: ' + data.main.temp + 'K\n' +
-     'Humidity: ' + data.main.humidity + '%\n' +
-	 'Wind: ' + data.wind.speed + 'km/h \n');   
-
-}
-});
-});
+$(document).ready(function() {
+  $('button').on('click', function() {
+    $.ajax({
+      type: 'GET',
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=Edinburgh&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
+      success: function weather(data) {
+        console.log(data);
+        alert('Location: ' + data.name + '\n' + 
+              'Temperature: ' + data.main.temp + 'K\n' + 
+              'Humidity: ' + data.main.humidity + '%\n' + 
+              'Wind: ' + data.wind.speed + 'km/h \n');
+      }
+    });
+  });
 });
