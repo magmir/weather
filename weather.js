ab5aafdd9d786f64990b80c4354aaf5ea36d21c9
mamir <magda.miranowicz@gmail.com>
2015-11-23 13:05:15 +0000

    Add functionality: selecting country from the list (JSON)

--- weather.js
+++ weather.js
@@ -1,15 +1,22 @@
 $(document).ready(function() {
-  $('button').on('click', function() {
-    $.ajax({
-      type: 'GET',
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=Edinburgh&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
-      success: function weather(data) {
-        console.log(data);
-        alert('Location: ' + data.name + '\n' + 
-              'Temperature: ' + data.main.temp + 'K\n' + 
-              'Humidity: ' + data.main.humidity + '%\n' + 
-              'Wind: ' + data.wind.speed + 'km/h \n');
-      }
+  $.getJSON("cities.json", function(records) {
+    $.each(records.cities, function(key, val) {
+      $('#city').append($('<option>').text(val.name).attr("id", val.name));
+               
     });
   });
+
+
+
+  $('button').on('click', function() {
+  $.ajax({
+  type: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q="+myCity+"&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
+  success: function weather(data) {
+  console.log(data);
+  alert('Location: ' + data.name + '\n' + 'Temperature: ' + data.main.temp + 'K\n' + 'Humidity: ' + data.main.humidity + '%\n' + 'Wind: ' + data.wind.speed + 'km/h \n');
+ }
+ });
+ });
 });
+
