ce356e82ea289a8607c200d1eb0333c1537cc904
mamir <magda.miranowicz@gmail.com>
2015-11-23 15:34:53 +0000

    Add functionality: displaying weather for selected country

--- weather.js
+++ weather.js
@@ -1,22 +1,36 @@
 $(document).ready(function() {
-  $.getJSON("cities.json", function(records) {
+
+  $.getJSON("cities.json", function(records) { 
     $.each(records.cities, function(key, val) {
       $('#city').append($('<option>').text(val.name).attr("id", val.name));
-               
+
     });
   });
 
+  $(function() { 
+    $('#city').change(function() {
+      localStorage.setItem('current', this.value);
+    });
+    if (localStorage.getItem('current')) {
+      $('#city').val(localStorage.getItem('current')).trigger('change');
+    }
+  });
 
 
   $('button').on('click', function() {
-  $.ajax({
-  type: 'GET',
-  url: 'http://api.openweathermap.org/data/2.5/weather?q="+myCity+"&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
-  success: function weather(data) {
-  console.log(data);
-  alert('Location: ' + data.name + '\n' + 'Temperature: ' + data.main.temp + 'K\n' + 'Humidity: ' + data.main.humidity + '%\n' + 'Wind: ' + data.wind.speed + 'km/h \n');
- }
- });
- });
-});
+    var myCity = localStorage.getItem('current');
+    console.log(myCity);
 
+    $.ajax({
+      type: 'GET',
+      url: 'http://api.openweathermap.org/data/2.5/weather?q=' + myCity + '&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
+      success: function weather(data) {
+        var temp = Math.round(data.main.temp - 273.15);
+        var html = 'Location: ' + data.name + '<br>' + 'Temperature: ' + temp + 'C<br>' + 'Humidity: ' + data.main.humidity + '%<br>' + 'Wind: ' + data.wind.speed + 'km/h \n';
+        $('#weather').html(html);
+       // html = '';
+
+      }
+    });
+  });
+});
