0f36fd9ad4282c890214dbe69a7d31c8c3602146
mamir <magda.miranowicz@gmail.com>
2015-11-25 14:07:55 +0000

    CSS styling

--- weather.js
+++ weather.js
@@ -14,50 +14,60 @@ $(document).ready(function() {
 
   $('button').on('click', function() {
     var myId = localStorage.getItem('current');
-  
-    $.ajax({
-      type: 'GET',
-      url: 'http://api.openweathermap.org/data/2.5/weather?id=' + myId + '&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
-      success: function weather(data) {
-        console.log(data);
-        var temp = Math.round(data.main.temp - 273.15);
-        var html = 'Current weather: <br>' + 
-        				 'Location: ' + data.name + '<br>' + 
-       				 'Weather: ' + data.weather[0].description + '<br>' + 
-       				 'Temperature: ' + temp + 'C<br>' + 'Humidity: ' + data.main.humidity + '%<br>' +
-       				  'Wind: ' + data.wind.speed + 'm/s';
-        $('#weather').html(html);
-        var imgdes = data.weather[0].icon;
-        $('.img1').empty();
-        $('.img1').append('<img src="img/' + imgdes + '.png"/>');
-      },
-      error: function() {
-        alert("Error");
-      }
-    });
-   
-    $.ajax({
+    $('.wth').addClass('well');  
 
+    var weatherForToday = function(data, day) {
+      console.log(data);
+      var myWeather = data.list[day];
+      var temp = Math.round(myWeather.main.temp - 273.15);
+      var date = (myWeather.dt_txt).slice(0, 10);
+      var html = 'Weather for: ' + date + '<br>' + 'Weather: ' + myWeather.weather[0].description + '<br>' + 'Temperature: ' + temp + '&deg;C<br>' + 'Humidity: ' + myWeather.main.humidity + '%<br>' + 'Wind: ' + myWeather.wind.speed + 'm/s';
+		    
+      $('#weather').html(html);
+      var imgdes = myWeather.weather[0].icon;
+      $('.img1').empty();
+      $('.img1').append('<img src="img/' + imgdes + '.png"/>');
+    };
+
+    var weatherForTomorrow = function(data, day) {
+      console.log(data);
+      var myWeather = data.list[day];
+      var temp = Math.round(myWeather.main.temp - 273.15);
+      var date = (myWeather.dt_txt).slice(0, 10);
+      var html = 'Weather for: ' + date + '<br>' + 'Weather: ' + myWeather.weather[0].description + '<br>' + 'Temperature: ' + temp + '&deg;C<br>' + 'Humidity: ' + myWeather.main.humidity + '%<br>' + 'Wind: ' + myWeather.wind.speed + 'm/s';
+      $('#forecast').html(html);
+      var imgdes = myWeather.weather[0].icon;
+      $('.img2').empty();
+      $('.img2').append('<img src="img/' + imgdes + '.png"/>');
+    };
+ var weatherForDayAfterTomorrow = function(data, day) {
+      console.log(data);
+      var myWeather = data.list[day];
+      var temp = Math.round(myWeather.main.temp - 273.15);
+      var date = (myWeather.dt_txt).slice(0, 10);
+      var html = 'Weather for: ' + date + '<br>' + 'Weather: ' + myWeather.weather[0].description + '<br>' + 'Temperature: ' + temp + '&deg;C<br>' + 'Humidity: ' + myWeather.main.humidity + '%<br>' + 'Wind: ' + myWeather.wind.speed + 'm/s';
+      $('#forecast2').html(html);
+      var imgdes = myWeather.weather[0].icon;
+      $('.img3').empty();
+      $('.img3').append('<img src="img/' + imgdes + '.png"/>');
+    };
+
+    $.ajax({
       type: 'GET',
       url: 'http://api.openweathermap.org/data/2.5/forecast?id=' + myId + '&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
       success: function weather(data) {
-        console.log(data);
-        var temp = Math.round(data.list[3].main.temp - 273.15);
-        var date=(data.list[3].dt_txt).slice(0,10);
-        var html = 'Weather for: ' + date +'<br>' +
-        				'Loation: ' + data.city.name + '<br>' + 
-        				 'Weather: ' + data.list[3].weather[0].description + '<br>' +        				 
-             		 'Temperature: ' + temp + 'C<br>' + 
-                   'Humidity: ' + data.list[3].main.humidity + '%<br>' + 
-                    'Wind: ' + data.list[3].wind.speed + 'm/s';
-                     $('#forecast').html(html);
-        var imgdes=data.list[3].weather[0].icon;
-        $('.img2').empty();        
-        $('.img2').append('<img src="img/' + imgdes + '.png"/>');
+        var today = 0;
+        var tomorrow = 8;
+        var dayAfterTomorrow=16;
+        weatherForToday(data, today);
+        weatherForTomorrow(data, tomorrow);
+        weatherForDayAfterTomorrow (data, dayAfterTomorrow);
       },
       error: function() {
         alert("Error");
       }
     });
+
+
   });
 });
