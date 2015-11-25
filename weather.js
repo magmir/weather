52797b97537eac86f308aae36ec6328eeb4c2b9c
mamir <magda.miranowicz@gmail.com>
2015-11-25 10:22:02 +0000

    Saving id instead of city name in local storage

--- weather.js
+++ weather.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.getJSON("cities.json", function(records) {
     $.each(records.cities, function(i, val) {
-      $('#city').append($('<option>').text(val.name).val(val.name));
+      $('#city').append($('<option>').text(val.name).val(val._id));
     });
     if (localStorage.getItem('current')) {
       $('#city').val(localStorage.getItem('current'));
@@ -13,53 +13,11 @@ $(document).ready(function() {
   });
 
   $('button').on('click', function() {
-    var myCity = localStorage.getItem('current');
-    var idlist = {
-      "Barcelona": {
-        "id": 6356055
-      },
-      "Berlin": {
-        "id": 2950159
-      },
-      "Budapest": {
-        "id": 3054643
-      },
-      "Edinburgh": {
-        "id": 2650225
-      },
-      "Frankfurt": {
-        "id": 2925533
-      },
-      "Krakow": {
-        "id": 3094802
-      },
-      "Lisbon": {
-        "id": 6458923
-      },
-      "London": {
-        "id": 2643743
-      },
-      "Sopot": {
-        "id": 7530820
-      },
-      "Sydney": {
-        "id": 2147714
-      },
-      "Warszawa": {
-        "id": 7531926
-      },
-      "Zakopane": {
-        "id": 7531513
-      },
-      "Zurich": {
-        "id": 2657896
-      }
-    };
-    var id = (idlist[myCity].id).toString();
-    
+    var myId = localStorage.getItem('current');
+  
     $.ajax({
       type: 'GET',
-      url: 'http://api.openweathermap.org/data/2.5/weather?q=' + myCity + '&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
+      url: 'http://api.openweathermap.org/data/2.5/weather?id=' + myId + '&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
       success: function weather(data) {
         console.log(data);
         var temp = Math.round(data.main.temp - 273.15);
@@ -77,10 +35,11 @@ $(document).ready(function() {
         alert("Error");
       }
     });
+   
     $.ajax({
 
       type: 'GET',
-      url: 'http://api.openweathermap.org/data/2.5/forecast?id=' + id + '&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
+      url: 'http://api.openweathermap.org/data/2.5/forecast?id=' + myId + '&APPID=06dcf0e82cb45b8b6964a33d135e1ed6',
       success: function weather(data) {
         console.log(data);
         var temp = Math.round(data.list[3].main.temp - 273.15);
